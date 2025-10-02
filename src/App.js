dc340a54039f952a170d0115beddccef8ba4bee7
Sreelal <sreelalseven@gmail.com>
2025-10-02 14:42:44 +0530

    navbar test 2

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
         <Header/>
         <Routes>
           <Route path='/' element={<Home/>} />
+          
         </Routes>
       </BrowserRouter>
     </div>
--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,43 +1,55 @@
-import { Button, Container, Nav, Navbar, Offcanvas } from "react-bootstrap";
+import { Button, Col, Container, Nav, Navbar, Offcanvas, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "../Styles/Header.css"
 
 
-const Header = ()=>{
-    return(
-        <Container className="p-0">
-            <Navbar expand="lg" className=" mt-3">
-          <Container fluid className="p-0">
-            <Navbar.Brand as={Link}>Travel <span className="tour-text-color">Tour</span></Navbar.Brand>
-            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-lg`} />
-            <Navbar.Offcanvas
-              id={`offcanvasNavbar-expand-lg`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-lg`}
-              placement="end"
-            >
-              <Offcanvas.Header closeButton>
-                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-lg`}>
-                  Travel Tour
-                </Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body>
-                <Nav className="justify-content-center flex-grow-1 pe-3 ">
-                  <Nav.Link href="#action1">Home</Nav.Link>
-                  <Nav.Link href="#action2">Pages</Nav.Link>
-                  <Nav.Link href="#action1">Tour List</Nav.Link>
-                  <Nav.Link href="#action2">Tour Search</Nav.Link>
-                  <Nav.Link href="#action2">Blog</Nav.Link>
-                  
-                </Nav>
-                
-                  <Button classnam>="log-btn"Login</Button>
-                
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
-          </Container>
-        </Navbar>
+const Header = () => {
+  return (
+    <Container className="py-0  px-4 px-md-0">
+      <Navbar expand="lg" className=" mt-3">
+        <Container fluid className="p-0">
+          <Navbar.Brand as={Link}>Travel <span className="tour-text-color">Tour</span></Navbar.Brand>
+          <Row>
+            <Col className=" d-block d-lg-none p-0"
+            ><Button className="log-btn">Login</Button></Col>
+            <Col>
+              <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-lg`} />
+              <Navbar.Offcanvas
+                id={`offcanvasNavbar-expand-lg`}
+                aria-labelledby={`offcanvasNavbarLabel-expand-lg`}
+                placement="end"
+              >
+                <Offcanvas.Header closeButton>
+                  <Offcanvas.Title id={`offcanvasNavbarLabel-expand-lg`}>
+                    Travel Tour
+                  </Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                  <Nav className="justify-content-center flex-grow-1 pe-3 ">
+                    
+                    <Nav.Link >Home</Nav.Link>
+                    <Nav.Link >Pages</Nav.Link>
+                    <Nav.Link >Tour List</Nav.Link>
+                    <Nav.Link >Tour Search</Nav.Link>
+                    <Nav.Link >Blog</Nav.Link>
+
+                  </Nav>
+
+
+
+                </Offcanvas.Body>
+
+              </Navbar.Offcanvas>
+            </Col>
+
+          </Row>
+
+          <Button className="log-btn d-none d-lg-block">Login</Button>
         </Container>
-    )
+
+      </Navbar>
+    </Container>
+  )
 }
 
 export default Header;
